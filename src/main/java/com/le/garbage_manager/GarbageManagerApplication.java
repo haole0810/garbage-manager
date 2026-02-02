4b9b24329f99492766d5ed9009646635795f0875
haole0810 <letanhao0810@gmail.com>
2026-02-02 14:15:53 +0700

    garbage-manager

---
+++ src/main/java/com/le/garbage_manager/GarbageManagerApplication.java
@@ -0,0 +1,13 @@
+package com.le.garbage_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarbageManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarbageManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/le/garbage_manager/GarbageManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.le.garbage_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarbageManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
